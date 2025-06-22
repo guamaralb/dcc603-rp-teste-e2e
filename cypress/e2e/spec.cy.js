e41ec14f4e3bb0e6e497a675a8aea7b33e417308
guamaralb <gu.amaralb@gmail.com>
2025-06-22 19:22:21 -0300

    Criados mais tres testes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -68,4 +68,66 @@ describe('TODOMvc App', () => {
       .children()
       .should('have.length', 2);
   });
+
+  it('Remove tarefas completadas', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input')
+      .type('Primeira tarefa{enter}')
+      .type('Segunda tarefa{enter}')
+
+    cy.get('[data-cy=toggle-todo-checkbox]')
+      .first()
+      .click();
+
+    cy.get('.clear-completed')
+      .click();
+
+    cy.get('[data-cy=todos-list]').children().should('have.length', 1)
+      .first()
+      .should('have.text', 'Segunda tarefa');
+  });
+
+  it('Edita uma tarefa', () => {
+    cy.visit('')
+
+    cy.get('[data-cy=todo-input]')
+      .as('input')
+      .type('Tarefa de exemplo{enter}')
+
+    cy.contains('[data-cy=todos-list] li', 'Tarefa de exemplo')
+      .as('tarefa')
+      .dblclick();
+
+    cy.get('@tarefa')
+      .find('.edit')
+      .as('inputEdicao')
+      .clear()
+      .type('Tarefa de exemplo modificada{enter}')
+
+    cy.get('[data-cy=todos-list] > li')
+      .should('contain.text', 'Tarefa de exemplo modificada')
+
+    cy.get('[data-cy=todos-list]')
+      .children()
+      .its('length')
+      .should('eq', 1)
+  })
+
+  it('Marca 3 tarefas como completas', () => {
+    cy.visit('');
+
+    cy.get('[data-cy=todo-input]')
+      .type('Tarefa 1{enter}')
+      .type('Tarefa 2{enter}')
+      .type('Tarefa 3{enter}')
+
+    cy.get('.toggle-all-label').click();
+
+    cy.get('[data-cy=todos-list] li')
+      .should('have.length', 3)
+      .each(($el) => {
+        cy.wrap($el).should('have.class', 'completed');
+      });
+  })
 });
